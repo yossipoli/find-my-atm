7bac3794f5bae0922d5b591e98c2314b1f35e897
Yossi <yossipoli@gmail.com>
2023-12-11 12:56:08 +0200

    add api to get cities, bank names and atm locations

---
+++ src/DAL/API.tsx
@@ -0,0 +1,40 @@
+import { ATM_type, BankName, City } from '../types/queries'
+
+const api_url = 'https://data.gov.il/api/3/action/datastore_search'
+const cities_resource = 'd4901968-dad3-4845-a9b0-a57d027f11ab'
+const banks_resource =
+	'ebb61778-e34c-4e67-8fcf-0e643d9cf8c2&q={"Category":"בנקים רגילים"}'
+const atm_resource = 'b9d690de-0a9c-45ef-9ced-3e5957776b26'
+
+const API = {
+	getCities: async (): Promise<string[]> => {
+		const cities = await fetch(
+			`${api_url}?resource_id=${cities_resource}&limit=15000`
+		).then((res) => res.json())
+		return cities.result.records.map((city: any) => city['שם_ישוב'].trim())
+	},
+
+	getBanks: async (): Promise<string[]> => {
+		const banks = await fetch(`${api_url}?resource_id=${banks_resource}`).then(
+			(res) => res.json()
+		)
+		return banks.result.records
+	},
+
+	getAtm: async (
+		city: City,
+		bankName: BankName,
+		type: ATM_type
+	): Promise<string[]> => {
+		const cityQuery = city ? `"City": "${city}",` : ''
+		const bankQuery = bankName ? `"Bank_Name": "${bankName}",` : ''
+		const atmQuery = type ? `ATM_Type": "${type}"` : ''
+		const q = `q={${cityQuery}${bankQuery}${atmQuery}}`
+		const atm = await fetch(`${api_url}?resource_id=${atm_resource}&${q}`).then(
+			(res) => res.json()
+		)
+		return atm.result.records
+	},
+}
+
+export default API
---
+++ src/types/queries.tsx
@@ -0,0 +1,5 @@
+import API from './../DAL/API'
+
+export type City = string
+export type ATM_type = 'משיכת מזומן' | 'מכשיר מידע/ואו מתן הוראות'
+export type BankName = string
